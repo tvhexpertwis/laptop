a10d36ec1d63d3d5f8bfa3177d9c023de1f8bb76
tvhexpertwis <50160881+tvhexpertwis@users.noreply.github.com>
2020-06-14 12:25:41 -0500

    Update Total.js

--- src/components/Total.js
+++ src/components/Total.js
@@ -5,7 +5,7 @@ function Total(props){
     
     return(
         <div className="summary__total__value">
-            <Summary currency={props.currency} total={props.total}/>
+//             <Summary currency={props.currency} total={props.total}/>
             {props.currency.format(props.total)}
         </div>
     )
